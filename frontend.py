d5e9a4dabb836820d721353098b9cea58982b33f
Sophie <sophietanker@gmail.com>
2023-04-11 14:40:07 -0400

    Advanced Settings and Settings Tab Setup

--- frontend.py
+++ frontend.py
@@ -4,41 +4,88 @@ import tkinter as tk
 import customtkinter as ctk
 import os
 
-#instantiate the main window. this will be cleared once the button is pressed.
-window = ctk.CTk()
-window.title("DeGenPrime")
-window.geometry("500x300")
-frame = Frame(window)
-frame.pack()
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("green")
 
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+#instantiate the main app. this will be cleared once the button is pressed.
+        self.title("DeGenPrime")
+        self.geometry("500x300")
+        frame = Frame(self)
+        frame.grid()
 
-#event handling for when the button is pressed.
-def handle_click(event):
-    lbl_greeting.destroy()
-    btn_greeting.destroy()
-    window.geometry("700x500")
-    btn_browsefiles.place(x=10,y=10)
+        #event handling for when the button is pressed.
+        def handle_click(event):
+            
+            # configure grid layout (4x4)
+            self.grid_columnconfigure((0,1,2,3), weight=1)
+            self.grid_rowconfigure((0, 1, 2), weight=1)
 
-#entry screen. this will describe to the user the purpose of DeGenPrime
-lbl_greeting = ctk.CTkLabel(text="Welcome to DeGenPrime. Our goal is to maximize your primer design experience while minimizing your headaches. Input either an unaligned or a prealigned .fasta or .fna file, and allow DeGenPrime to recommend the best primers for your PCR use.", master = window,wraplength = 450, justify= "center")
-lbl_greeting.pack(fill=BOTH, expand=True)
-btn_greeting = ctk.CTkButton(master = window,text="ENTER")
-btn_greeting.bind("<Button-1>", handle_click)
-btn_greeting.pack()
+            #delete old info
+            self.btn_greeting.destroy()
+            self.lbl_greeting.destroy()
 
-#event handling for the user to input their file
-def find_input_file():
-    file = askopenfile(mode ='r', filetypes =[('Acceptable Filetypes', '*.fasta,*.fna,*.clust,*.faa')])
-    if file:
-        inputpath = os.path.abspath(file.name)
-        lbl_inputpath = ctk.CTkLabel(master = window, text="Your selected file is: " + str(inputpath)).place(x=10,y=50)
-        filecontent = file.read()
+            def find_input_file():
+                file = askopenfile(mode ='r', filetypes =[('Acceptable Filetypes', '*.fasta,*.fna,*.clust,*.faa')])
+                if file:
+                    inputpath = os.path.abspath(file.name)
+                    self.lbl_inputpath = ctk.CTkLabel(self.sidebar_frame, text="Your selected file is: " + str(inputpath),wraplength = 140,justify="center")
+                    self.lbl_inputpath.grid(row=2, column=0, padx=20)
+                    filecontent = file.read()
 
-        #somewhere in this event handling scheme we will have to pass the file to C++. figure out how to do this.
+            #create the frame for the three side buttons -- browse, run, and save
+            self.sidebar_frame = ctk.CTkFrame(self, width=140, corner_radius=0)
+            self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+            self.sidebar_frame.grid_rowconfigure(4, weight=1)
+            self.btn_browse = ctk.CTkButton(self.sidebar_frame, text = "BROWSE FILES", command = find_input_file)
+            self.btn_browse.grid(row=0, column=0, padx=20, pady=30)
+            self.btn_run = ctk.CTkButton(self.sidebar_frame, text = "RUN PROGRAM")
+            self.btn_run.grid(row=1, column=0, padx=20, pady=30)
+            self.btn_save = ctk.CTkButton(self.sidebar_frame, text = "SAVE SETTINGS")
+            self.btn_save.grid(row=2, column=0, padx=20, pady=30)
 
+            self.grid_rowconfigure(0, weight=1)
+            self.grid_columnconfigure((0, 1, 2), weight=1)
 
-btn_browsefiles = ctk.CTkButton(master=window,text="Browse Files", command=find_input_file)
+            #create the frame for tabview (settings and advanced settings)
+            self.settings_tabview = ctk.CTkTabview(self, width=250)
+            self.settings_tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+            self.settings_tabview.add("Settings")
+            self.settings_tabview.add("Advanced Settings")
 
+            self.amplicon = ctk.CTkEntry(self.settings_tabview.tab("Settings"), placeholder_text="Amplicon Length")
+            self.amplicon.grid(row = 0, column = 0,padx=20, pady=(20, 10))
+            self.ampl_label = ctk.CTkLabel(self.settings_tabview.tab("Settings"), text= "Base Pairs")
+            self.ampl_label.grid(row = 0, column = 2,pady=(20, 10))
+            self.DNAconc = ctk.CTkEntry(self.settings_tabview.tab("Settings"), placeholder_text="DNA Concentration")
+            self.DNAconc.grid(row = 1, column = 0,padx=20, pady=(20, 10))
+            self.DNAconc_label = ctk.CTkLabel(self.settings_tabview.tab("Settings"), text= "mMol")
+            self.DNAconc_label.grid(row = 1, column = 2,pady=(20, 10))
+            self.temp = ctk.CTkEntry(self.settings_tabview.tab("Settings"), placeholder_text="Temperature")
+            self.temp.grid(row = 2, column = 0,padx=20, pady=(20, 10))
+            self.temp_label = ctk.CTkLabel(self.settings_tabview.tab("Settings"), text= "C°")
+            self.temp_label.grid(row = 2, column = 2,pady=(20, 10))
 
+            self.primer_conc = ctk.CTkEntry(self.settings_tabview.tab("Advanced Settings"), placeholder_text="Primer Concentration")
+            self.primer_conc.grid(row = 0, column = 0,padx=20, pady=(20, 10))
+            self.primer_conc_label = ctk.CTkLabel(self.settings_tabview.tab("Advanced Settings"), text= "nMol")
+            self.primer_conc_label.grid(row = 0, column = 2,pady=(20, 10))
+            self.mono_conc = ctk.CTkEntry(self.settings_tabview.tab("Advanced Settings"), placeholder_text="Monovalent Ion Concentration")
+            self.mono_conc.grid(row = 1, column = 0,padx=20, pady=(20, 10))
+            self.mono_conc_label = ctk.CTkLabel(self.settings_tabview.tab("Advanced Settings"), text= "mMol")
+            self.mono_conc_label.grid(row = 1, column = 2,pady=(20, 10))
 
-window.mainloop()
+        self.sidebar_frame = ctk.CTkFrame(self, width = 500)
+        self.lbl_greeting = ctk.CTkLabel(self,text="Welcome to DeGenPrime. Our goal is to maximize your primer design experience while minimizing your headaches. "
+                                         + "Input either an unaligned or a prealigned .fasta or .fna file, and allow DeGenPrime to recommend the best primers for your PCR use."
+                                          ,wraplength = 450, justify= "center")
+        self.lbl_greeting.grid(padx=30, pady=50)
+        self.btn_greeting = ctk.CTkButton(self,text="ENTER")
+        self.btn_greeting.bind("<Button-1>", handle_click)
+        self.btn_greeting.grid(padx=30, pady=10)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
